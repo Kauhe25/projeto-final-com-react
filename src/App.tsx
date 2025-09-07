325b596cf210e1a9e733b011e33c75ee5dac9ed7
kauhe <kauhe25@gmail.com>
2025-09-07 16:29:55 -0300

    feat: usando o tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,26 @@
 
 function App() {
   return (
-    <>
+    <div className="p-10 bg-cyan-950 text-white">
       <h1>Projeto final em react</h1>
+      <h2 className="text-yellow-300">Consumindo API rest</h2>
       <hr />
-    </>
+      <button className="
+      px-4 
+      py-2 
+      bg-cyan-300 
+      rounded-xl 
+      mt-4
+      text-cyan-950 
+      border-2 
+      cursor-pointer
+      hover: border-cyan-300
+      hover:bg-cyan-950
+      hover:text-white
+      ">
+        Enviar
+      </button>
+    </div>
   )
 }
 
