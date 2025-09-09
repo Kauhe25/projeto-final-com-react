7f0718009c7d75419b7bdfd8fa937fb3f8a2cf9f
kauhe <kauhe25@gmail.com>
2025-09-09 00:36:14 -0300

    feat: exemplos de chamadas de rotas com link e navigate

--- src/Components/Menu/index.tsx
+++ src/Components/Menu/index.tsx
@@ -1,10 +1,26 @@
 import { MegaMenu } from "primereact/megamenu";
 import menuItems from "./menuItems";
+import { Link, useNavigate } from "react-router";
+import { Button } from "primereact/button";
 
 const Menu = () => {
+
+  // Serve para navegar nos links das rotas via JS
+  const navigate = useNavigate()
   return (
     <>
-       <MegaMenu model={menuItems()} breakpoint="900px" />
+      <MegaMenu model={menuItems()} breakpoint="900px" />
+      <hr />
+      <a href="/departamentos">Departamentos</a>
+      <hr />
+      <Link to="/departamentos">Departamentos</Link>
+      <hr />
+      <Button severity="danger" label="Departamentos" 
+        onClick={() => {
+          navigate('/departamentos')
+        }}
+      />
+
     </>
   )
 }
